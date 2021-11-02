c5a76de67e6d753b9229cd39292f78de3686d809
Matúš Korman <matus.korman2004@gmail.com>
2021-11-02 18:57:29 +0100

    no message

---
+++ Druhá úloha git/Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	cout << "Informacie o mne:" << endl;
+	cout << "Meno: Matus" << endl;
+	cout << "Priezvisko: Korman" << endl;
+	cout << "Adresa: Dolinsky potok" << endl;
+	cout << "Trieda: 2.AI" << endl;
+	cout << "Vek: 16" << endl;
+
+	return 0;
+
+}
