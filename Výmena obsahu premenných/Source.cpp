b53088bb4cb478d2d18191fc0982cb78f78d240e
Matúš Korman <matus.korman2004@gmail.com>
2021-11-02 19:33:11 +0100

    Výmena premenných

---
+++ Výmena obsahu premenných/Source.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a, b;
+	a = 10;
+	b = 99;
+	cout << "A=" << a << endl;
+	cout << "B=" << b << endl;
+	cout << "Novy obsah premennych." << endl;
+	int noveA, noveB;
+	noveA = 99;
+	noveB = 10;
+	cout << "A=" << noveA << endl;
+	cout << "B=" << noveB << endl;
+
+	return 0;
+
+}
