17bc42c98b6c84c84b18d92b735386bc6be51fae
MatusKorman <91270622+MatusKorman@users.noreply.github.com>
2022-04-03 14:02:07 +0200

    Add files via upload

--- Piškvorky/Source.cpp
+++ Piškvorky/Source.cpp
@@ -1,132 +1,203 @@
 #include <iostream>
+#include <conio.h>
+
 using namespace std;
 
-char ocislovanie[10] = { 'o','1','2','3','4','5','6','7','8','9' };
+int checkwin(char[]);
+void board(char[]);
 
-int vysledok();
-void tabulka();
-
-int main()
+void main()
 {
-    int Hrac1 = 1, i, choice;
+	char square[10] = { 'o','1','2','3','4','5','6','7','8','9' };
+	int player = 1, i, choice;
 
-    char znak;
-    do
-    {
-        tabulka();
-        Hrac1 = (Hrac1 % 2) ? 1 : 2;
+	char mark;
+	do
+	{
+		board(square);
 
-        cout << "Hrac " << Hrac1 << " zadaj cislo: ";
-        cin >> choice;
+		if (player % 2 == 1)
+			player = 1;
+		else
+			player = 2;
 
-        znak = (Hrac1 == 1) ? 'X' : 'O';
+		if (player == 2)
+		{
+			cout << "Zmackni ENTER pre PC" << endl;
+			choice = rand() % 9;
+			mark = 'O';
+			int turn = 1;
+			int placed = 0;
+			while (placed == 0)
+			{
 
-        if (choice == 1 && ocislovanie[1] == '1')
+				if (choice == 1 && square[1] == '1') {
+					square[1] = mark;
+					placed = 1;
+				}
+				else if (choice == 2 && square[2] == '2') {
 
-            ocislovanie[1] = znak;
-        else if (choice == 2 && ocislovanie[2] == '2')
+					square[2] = mark;
+					placed = 1;
+				}
+				else if (choice == 3 && square[3] == '3') {
 
-            ocislovanie[2] = znak;
-        else if (choice == 3 && ocislovanie[3] == '3')
+					square[3] = mark;
+					placed = 1;
+				}
+				else if (choice == 4 && square[4] == '4') {
 
-            ocislovanie[3] = znak;
-        else if (choice == 4 && ocislovanie[4] == '4')
+					placed = 1;
+					square[4] = mark;
+				}
+				else if (choice == 5 && square[5] == '5') {
 
-            ocislovanie[4] = znak;
-        else if (choice == 5 && ocislovanie[5] == '5')
+					square[5] = mark;
+					placed = 1;
+				}
+				else if (choice == 6 && square[6] == '6') {
 
-            ocislovanie[5] = znak;
-        else if (choice == 6 && ocislovanie[6] == '6')
+					square[6] = mark;
+					placed = 1;
+				}
+				else if (choice == 7 && square[7] == '7') {
 
-            ocislovanie[6] = znak;
-        else if (choice == 7 && ocislovanie[7] == '7')
+					square[7] = mark;
+					placed = 1;
+				}
+				else if (choice == 8 && square[8] == '8') {
 
-            ocislovanie[7] = znak;
-        else if (choice == 8 && ocislovanie[8] == '8')
+					square[8] = mark;
+					placed = 1;
+				}
+				else if (choice == 9 && square[9] == '9') {
 
-            ocislovanie[8] = znak;
-        else if (choice == 9 && ocislovanie[9] == '9')
+					square[9] = mark;
+					placed = 1;
+				}
 
-            ocislovanie[9] = znak;
-        else
-        {
-            cout << "Zle cislo!";
+				else
+				{
+					choice = rand() % 9;
+				}
+			}
+			i = checkwin(square);
+			player++;
+			_getche();
+			board(square);
+		}
 
-            Hrac1--;
-            cin.ignore();
-            cin.get();
-        }
-        i = vysledok();
 
-        Hrac1++;
-    } while (i == -1);
-    tabulka();
-    if (i == 1)
+		else if (player == 1)
+		{
+			cout << "Vyber si cislo" << " :  ";
+			cin >> choice;
+			mark = 'X';
 
-        cout << "==>Hrac " << --Hrac1 << " vyhral." << endl;
-    else
-        cout << "==>\Remiza" << endl;
+			if (choice == 1 && square[1] == '1')
 
-    cin.ignore();
-    cin.get();
-    return 0;
+				square[1] = mark;
+			else if (choice == 2 && square[2] == '2')
+
+				square[2] = mark;
+			else if (choice == 3 && square[3] == '3')
+
+				square[3] = mark;
+			else if (choice == 4 && square[4] == '4')
+
+				square[4] = mark;
+			else if (choice == 5 && square[5] == '5')
+
+				square[5] = mark;
+			else if (choice == 6 && square[6] == '6')
+
+				square[6] = mark;
+			else if (choice == 7 && square[7] == '7')
+
+				square[7] = mark;
+			else if (choice == 8 && square[8] == '8')
+
+				square[8] = mark;
+			else if (choice == 9 && square[9] == '9')
+
+				square[9] = mark;
+			else
+			{
+				cout << "ZLY TAH! ";
+
+				player--;
+				_getche();
+			}
+			i = checkwin(square);
+
+			player++;
+		}
+	} while (i == -1);
+	board(square);
+	if (i == 1)
+
+		cout << "Gratulujem! Hrac " << --player << " vyhral. ";
+	else
+		cout << "REMIZA.";
+
+	_getche();
 }
 
-int vysledok()
+void board(char square[])
 {
-    if (ocislovanie[1] == ocislovanie[2] && ocislovanie[2] == ocislovanie[3])
+	system("cls");
+	cout << "                                          |-----------| " << endl;
+	cout << "                                          | Piskvorky | " << endl;
+	cout << "                                          |-----------| " << endl;
 
-        return 1;
-    else if (ocislovanie[4] == ocislovanie[5] && ocislovanie[5] == ocislovanie[6])
+	cout << "                                             |     |     " << endl;
+	cout << "                                          " << square[1] << "  |  " << square[2] << "  |  " << square[3] << endl;
 
-        return 1;
-    else if (ocislovanie[7] == ocislovanie[8] && ocislovanie[8] == ocislovanie[9])
+	cout << "--> Ty mas (X)                          _____|_____|_____" << endl;
+	cout << "                                             |     |     " << endl;
 
-        return 1;
-    else if (ocislovanie[1] == ocislovanie[4] && ocislovanie[4] == ocislovanie[7])
+	cout << "                                          " << square[4] << "  |  " << square[5] << "  |  " << square[6] << endl;
 
-        return 1;
-    else if (ocislovanie[2] == ocislovanie[5] && ocislovanie[5] == ocislovanie[8])
+	cout << "--> PC ma (O)                           _____|_____|_____" << endl;
+	cout << "                                             |     |     " << endl;
 
-        return 1;
-    else if (ocislovanie[3] == ocislovanie[6] && ocislovanie[6] == ocislovanie[9])
+	cout << "                                          " << square[7] << "  |  " << square[8] << "  |  " << square[9] << endl;
 
-        return 1;
-    else if (ocislovanie[1] == ocislovanie[5] && ocislovanie[5] == ocislovanie[9])
-
-        return 1;
-    else if (ocislovanie[3] == ocislovanie[5] && ocislovanie[5] == ocislovanie[7])
-
-        return 1;
-    else if (ocislovanie[1] != '1' && ocislovanie[2] != '2' && ocislovanie[3] != '3'
-        && ocislovanie[4] != '4' && ocislovanie[5] != '5' && ocislovanie[6] != '6'
-        && ocislovanie[7] != '7' && ocislovanie[8] != '8' && ocislovanie[9] != '9')
-
-        return 0;
-    else
-        return -1;
+	cout << "                                             |     |     " << endl << endl;
 }
 
-void tabulka()
+
+int checkwin(char square[])
 {
-    system("cls");
-    cout << "                                 |-----------|" << endl;
-    cout << "                                 | PISKVORKY |" << endl;
-    cout << "                                 |-----------|" << endl;
-    cout << endl;
-    cout << "                                    |     |     " << endl;
-    cout << "                                 " << ocislovanie[1] << "  |  " << ocislovanie[2] << "  |  " << ocislovanie[3] << endl;
+	if (square[1] == square[2] && square[2] == square[3])
 
-    cout << "|Hrac prvy ma X|               _____|_____|_____" << endl;
-    cout << "                                    |     |     " << endl;
-     
-    cout << "                                 " << ocislovanie[4] << "  |  " << ocislovanie[5] << "  |  " << ocislovanie[6] << endl;
+		return 1;
+	else if (square[4] == square[5] && square[5] == square[6])
 
-    cout << "|Hrac druhy ma O|              _____|_____|_____" << endl;
-    cout << "                                    |     |     " << endl;
+		return 1;
+	else if (square[7] == square[8] && square[8] == square[9])
 
-    cout << "                                 " << ocislovanie[7] << "  |  " << ocislovanie[8] << "  |  " << ocislovanie[9] << endl;
+		return 1;
+	else if (square[1] == square[4] && square[4] == square[7])
 
-    cout << "                                    |     |     " << endl;
-   
+		return 1;
+	else if (square[2] == square[5] && square[5] == square[8])
+
+		return 1;
+	else if (square[3] == square[6] && square[6] == square[9])
+
+		return 1;
+	else if (square[1] == square[5] && square[5] == square[9])
+
+		return 1;
+	else if (square[3] == square[5] && square[5] == square[7])
+
+		return 1;
+	else if (square[1] != '1' && square[2] != '2' && square[3] != '3'
+		&& square[4] != '4' && square[5] != '5' && square[6] != '6'
+		&& square[7] != '7' && square[8] != '8' && square[9] != '9')
+
+		return 0;
+	else
+		return -1;
 }
